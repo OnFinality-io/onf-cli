6235f8e95ad4a5deeb0a175ce8bc4b15208d48e8
Nihal <70577135+NihalKPatel@users.noreply.github.com>
2021-06-29 12:54:53 +1200

    Created support for updating network-spec's metadata

---
+++ cmd/networkspec/update.go
@@ -0,0 +1,43 @@
+package networkspec
+
+import (
+	"fmt"
+	"github.com/OnFinality-io/onf-cli/cmd/helpers"
+	"github.com/OnFinality-io/onf-cli/pkg/printer"
+	"github.com/OnFinality-io/onf-cli/pkg/service"
+	"github.com/spf13/cobra"
+)
+
+func UpdateCmd() *cobra.Command {
+	printFlags := printer.NewPrintFlags()
+	c := &cobra.Command{
+		Use:   "update",
+		Short: "update your network specs",
+		Run: func(cmd *cobra.Command, args []string) {
+			wsID, err := helpers.GetWorkspaceID(cmd)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			payload := &service.NetworkSpecMetadata{}
+			if filePath != "" {
+				err = helpers.ApplyDefinitionFile(filePath, payload)
+				if err != nil {
+					fmt.Println(err.Error())
+					return
+				}
+			}
+			err = service.UpdateNetworkSpecMetadata(wsID, networkID,payload)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			fmt.Println("Successfully update Network, #ID:", networkID)
+		},
+	}
+	c.Flags().StringVarP(&filePath, "file", "f", "", "definition file for update node, yaml or json")
+	c.Flags().StringVarP(&networkID, "network", "n", "", "Network id")
+	_ = c.MarkFlagRequired("network")
+	printFlags.AddFlags(c)
+	return c
+}
