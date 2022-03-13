cda179b0276ca6433cf3d953d79251db6bf7346e
Val Lisandro Dolot <vldolot@gmail.com>
2022-03-14 11:13:11 +1300

    To add a warning message when the provided storage size is less than the recommended backup storage size (#27)
    * OP-1305
    
    modified:   cmd/node/create.go
    - to add a warning message when the provided storage size is less than the recommended backup storage size
    
    modified:   pkg/service/networkspec.go
    - added GetNodeTypeFromPruningMode() into service pkg as a method of Backups
    
    * - bump up the version to 0.3.8
    
    * OP-1305
    
    modified:   cmd/node/create.go
    - to skip displaying the warning message if not initializing from backup
    
    Co-authored-by: vldolot <val.dolot@onfinality.io>

--- cmd/node/create.go
+++ cmd/node/create.go
@@ -2,10 +2,13 @@ package node
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/OnFinality-io/onf-cli/cmd/helpers"
 	"github.com/OnFinality-io/onf-cli/pkg/printer"
 	"github.com/OnFinality-io/onf-cli/pkg/service"
+	"github.com/OnFinality-io/onf-cli/pkg/utils"
 	"github.com/OnFinality-io/onf-cli/pkg/watcher"
 	"github.com/spf13/cobra"
 )
@@ -30,6 +33,14 @@ func createCmd() *cobra.Command {
 				fmt.Println(err.Error())
 				return
 			}
+
+			if payload.InitFromBackup { // skip this step if not initializing from backup
+				isBelowMinStorage, minStorageSize := isBackupStorageSizeBelow(payload)
+				if isBelowMinStorage {
+					fmt.Printf("Warning: Minimum %dGi required to use Lightning Restore/Backups\n", minStorageSize)
+				}
+			}
+			
 			// todo - filter out the minimum parameters can be put on the arguments instead of using a definition file
 			node, err := service.CreateNode(wsID, payload)
 			if err != nil {
@@ -57,4 +68,36 @@ func createCmd() *cobra.Command {
 	watcherFlags.AddFlags(c, "Watch for creation status")
 	printFlags.AddFlags(c)
 	return c
+}
+
+// to check if the provided storage size is valid for backups enabled networks
+// based on the combination of nodeType, clusterKey & networkSpecKey values
+func isBackupStorageSizeBelow(payload *service.CreateNodePayload) (bool, int) {
+	nodeType := string(payload.NodeType)
+	clusterKey := payload.ClusterHash
+	networkSpecKey := payload.NetworkSpecKey
+	storageSize, err := strconv.ParseUint(strings.TrimSuffix(*payload.Storage, "Gi"), 10, 32)
+	if err != nil {
+		fmt.Println(err.Error())
+		return false, -1
+	}
+
+	// get backups supported public networks data
+	backups, err := service.GetBackups()
+	if err != nil {
+		fmt.Println(err.Error())
+		return false, -1
+	}
+	
+	for _, b := range backups {
+		convertedNodeType := b.GetNodeTypeFromPruningMode()
+		// filter out by networks with backups only, by cluster key and by node type
+		if networkSpecKey == b.NetworkSpec && clusterKey == b.ClusterHash && utils.Contains(convertedNodeType, nodeType) { 
+			if uint(storageSize) < b.StorageSize {
+				return true, int(b.StorageSize)
+			}
+		}
+	}
+
+	return false, -1
 }
